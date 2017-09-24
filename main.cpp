233b890b844c60cb8fe9611455e00aaee12a4993
Oguz Meteer <info@guztech.nl>
2017-09-24 20:24:47 +0200

    Add "map" include to main.h
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <yaml-cpp/yaml.h>
-#include <map>
 
 using namespace std;
 
--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <memory>
 #include <algorithm>
 #include <vector>
+#include <map>
 
 class Component;
 class HalfAdder;
