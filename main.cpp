e74e635b5c42227d9cb46bd4e66fdf3976fc7231
Oguz Meteer <info@guztech.nl>
2017-10-05 12:40:33 +0200

    Remove unused code
    Signed-off-by: Oguz Meteer <info@guztech.nl>

--- main.cpp
+++ main.cpp
@@ -397,7 +397,6 @@ void ParseStimuli(System &system, YAML::Node config) {
 }
 
 int main(int argc, char **argv) {
-#if 1
 	map<std::string, comp_t> comps;
 	YAML::Node config;
 	System system;
@@ -471,96 +470,6 @@ int main(int argc, char **argv) {
 				 << "\": " << ow->GetValue() << "\n";
 		}
 	}
-#else
-	auto A0 = make_shared<Wire>();
-	auto B0 = make_shared<Wire>();
-	auto A1 = make_shared<Wire>();
-	auto B1 = make_shared<Wire>();
 
-	auto S0 = make_shared<Wire>();
-	auto S1 = make_shared<Wire>();
-	auto C0 = make_shared<Wire>();
-	auto C1 = make_shared<Wire>();
-	auto C2 = make_shared<Wire>();
-
-	auto fa1 = make_shared<FullAdder>();
-	auto fa2 = make_shared<FullAdder>();
-
-	fa1->Connect(FullAdder::PORTS::A, A0);
-	fa1->Connect(FullAdder::PORTS::B, B0);
-	fa2->Connect(FullAdder::PORTS::A, A1);
-	fa2->Connect(FullAdder::PORTS::B, B1);
-	fa1->Connect(FullAdder::PORTS::S, S0);
-	fa2->Connect(FullAdder::PORTS::S, S1);
-	fa1->Connect(FullAdder::PORTS::Cin, C0);
-	fa1->Connect(FullAdder::PORTS::Cout, C1);
-	fa2->Connect(FullAdder::PORTS::Cin, C1);
-	fa2->Connect(FullAdder::PORTS::Cout, C2);
-
-	System system;
-	system.AddComponent(fa1);
-	system.AddComponent(fa2);
-
-	C0->SetValue(true);
-	A0->SetValue(true);
-	B0->SetValue(true);
-	A1->SetValue(true);
-	B1->SetValue(true);
-
-	system.Update();
-
-	C0->SetValue(false);
-	A0->SetValue(false);
-	B0->SetValue(false);
-	A1->SetValue(false);
-	B1->SetValue(false);
-
-	system.Update();
-
-	cout << "Full adder:\nInput: " <<
-		A1->GetValue() << A0->GetValue() <<	" + " <<
-		B1->GetValue() << B0->GetValue() <<	" + " <<
-		C0->GetValue() << " = " <<
-		C2->GetValue() << S1->GetValue() << S0->GetValue() <<
-		"\nNumber of toggled bits: " << system.GetNumToggles() << "\n";
-
-/*
-	auto A = make_shared<Wire>();
-	auto B = make_shared<Wire>();
-	auto S = make_shared<Wire>();
-	auto C = make_shared<Wire>();
-
-	auto ha1 = make_shared<HalfAdder>();
-
-	A->SetDrives(ha1);
-	B->SetDrives(ha1);
-
-	ha1->Connect(HalfAdder::PORTS::A, A);
-	ha1->Connect(HalfAdder::PORTS::B, B);
-	ha1->Connect(HalfAdder::PORTS::S, S);
-	ha1->Connect(HalfAdder::PORTS::C, C);
-
-	bool a, b;
-
-	cout << "\nHalf adder:\nA: ";
-	while (!(cin >> a)) {
-		cin.clear();
-		cout << "Please enter a boolean value.\nA: ";
-	}
-
-	cout << "B: ";
-	while (!(cin >> b)) {
-		cin.clear();
-		cout << "Please enter a boolean value.\nB: ";
-	}
-
-	A->SetValue(true);
-	B->SetValue(true);
-
-	ha1->Update();
-
-	cout << "S: " << S->GetValue() << " C: " << C->GetValue() << "\n";
-*/
-#endif
 	return 0;
 }
